74f2f385232e46505a980a0cccc71648816fc400
Abderrahim Khali Ali <abderrahimkhaliali@gmail.com>
2026-04-01 10:53:37 +0100

    Reglage de l'environnement, Installer les depedencies.

---
+++ frontend/src/components/Pages/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div data-bs-page="navbar">
+        <nav>
+            <ul>
+                
+            </ul>
+            <ul>
+
+            </ul>
+            <ul>
+
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
