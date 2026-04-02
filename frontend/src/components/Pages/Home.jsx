08c7ec15ee3da83ae188c5c604bb3a09a285b299
Abderrahim Khali Ali <abderrahimkhaliali@gmail.com>
2026-04-02 11:44:32 +0100

    feat: mise en place du carrousel dynamique avec Swiper.js et contenu statique

--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -1,14 +1,65 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useNavigate } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper/modules';
 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 const Home = () => {
+  const navigate = useNavigate();
+  const homeTexts = {
+    heroTitle: "L'Excellence Technologique au Service de l'Industrie",
+    heroDescription: "Expertise, innovation et fiabilité : découvrez notre large gamme de produits et services technologiques."
+  }
   return (
     <div data-bs-page="home">
-        <Navbar />
-
-
-        <Footer />
+      <Navbar />
+      {/* <div className="div2">
+        <div className="container-fluid p-0">
+          <>
+            <h1 className='hero-title' style={{ fontWeight: "700" }}>{homeTexts.heroTitle}</h1>
+            <p id="quote" className="hero-description mt-2 mb-2">
+              {homeTexts.heroDescription}
+            </p>
+            <button className='btn voir-produits'>Voir nos produits <FiArrowRight /></button>
+          </>
+        </div>
+        <br />
+      </div> */}
+      <div className="div2">
+        <Swiper
+          modules={[Autoplay, EffectFade]}
+          effect='fade'
+          allowTouchMove={false}
+          className='hero-bg-swiper'
+          style={{ height: '100%', width: '100%' }}
+          autoplay={{ delay: 4000, disableOnInteraction: false }}
+        >
+          <SwiperSlide>
+            <div className='slide-img' style={{ backgroundImage: "url('/outils/backgrounds/iwaco.png')" }}></div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className='slide-img' style={{ backgroundImage: "url('/outils/backgrounds/b1.jpg')" }}></div>
+          </SwiperSlide>
+        </Swiper>
+        <div className="hero-static-content">
+          <div className="container-fluid p-0">
+            <>
+              <h1 className='hero-title' style={{ fontWeight: "700" }}>{homeTexts.heroTitle}</h1>
+              <p id="quote" className="hero-description mt-2 mb-2">
+                {homeTexts.heroDescription}
+              </p>
+              <button className='btn voir-produits'>Voir nos produits <FiArrowRight /></button>
+            </>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
